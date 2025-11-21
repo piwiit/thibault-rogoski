a21a84acdacc7d04247414c8f11af6aaf991acce
piwiit <bouillaux@hotmail.fr>
2025-11-21 17:02:30 +0100

    remove any in request prisma

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
             );
         }
 
-        const user = await (prisma as any).user.findUnique({
+        const user = await prisma.user.findUnique({
             where: { id: userId },
             select: { id: true, username: true },
         });
