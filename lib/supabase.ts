1bf7d7622d9fecf860cd1615917db674053f93b2
piwiit <bouillaux@hotmail.fr>
2026-06-17 19:58:03 +0200

    add supabase.ts to the list of recently edited files.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export function supabase() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
+  if (!supabaseUrl || !supabaseServiceKey) {
     throw new Error('Supabase credentials are not defined in environment variables.');
-}
+  }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  return createClient(supabaseUrl, supabaseServiceKey);
+}
